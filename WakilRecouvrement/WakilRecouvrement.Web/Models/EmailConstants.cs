f3a7abfb08b52dbf056f2f0f23deb969d4a70052
MalekBelayeb <malek.belayeb@esprit.tn>
2020-12-21 00:59:13 +0100

    prod

--- WakilRecouvrement/WakilRecouvrement.Web/Models/EmailConstants.cs
+++ WakilRecouvrement/WakilRecouvrement.Web/Models/EmailConstants.cs
@@ -8,13 +8,13 @@ namespace WakilRecouvrement.Web.Models
     public class EmailConstants
     {
 
-        public const string VERSEMENT_BODY = "Ci-joint vous trouverez une liste contenant des nouvelles versements"; 
-        public const string AVERIFIE_BODY = "Ci-joint vous trouverez une liste contenant des clients a verifé"; 
-        public const string ENCOURS_BODY = "Ci-joint vous trouverez une liste contenant des clients en cours de traitement"; 
-        public const string RDV_BODY = "Ci-joint vous trouverez une liste contenant des nouveaux rendez-vous"; 
+        public const string VERSEMENT_BODY = "Ci-joint vous trouverez une liste des nouveaux versements"; 
+        public const string AVERIFIE_BODY = "Ci-joint vous trouverez une liste des clients à verifier"; 
+        public const string ENCOURS_BODY = "Ci-joint vous trouverez une liste des clients en cours de traitement"; 
+        public const string RDV_BODY = "Ci-joint vous trouverez une liste des nouveaux rendez-vous"; 
         
         public const string VERSEMENT_SUBJECT = "Liste des versements";
-        public const string AVERIFIE_SUBJECT = "Liste des clients a verifié";
+        public const string AVERIFIE_SUBJECT = "Liste des clients à verifier";
         public const string ENCOURS_SUBJECT = "Liste des clients en cours de traitement";
         public const string RDV_SUBJECT = "Liste des rendez-vous";
        
