312d8b4ea1de9753805a08f1f3bee061109d994c
MalekBelayeb <malek.belayeb@esprit.tn>
2020-12-25 10:32:09 +0100

    gerer lots +modifier FN

---
+++ WakilRecouvrement/WakilRecouvrement.Web/Models/TelFN.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WakilRecouvrement.Web.Models
+{
+    public class TelFN
+    {
+        public bool TelPortableFN { get; set; }
+        public bool TelFixeFN { get; set; }
+    }
+}
