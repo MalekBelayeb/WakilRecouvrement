9e2e4360efb23723dbf1402d63b43a1ad4939858
MalekBelayeb <malek.belayeb@esprit.tn>
2021-01-04 16:25:53 +0100

    fix

--- WakilRecouvrement/WakilRecouvrement.Web/Models/EmailConstants.cs
+++ WakilRecouvrement/WakilRecouvrement.Web/Models/EmailConstants.cs
@@ -12,13 +12,11 @@ namespace WakilRecouvrement.Web.Models
         public const string AVERIFIE_BODY = "Veuillez me vérifier ces comptes svp."; 
         public const string ENCOURS_BODY = "Veuillez trouvez ci-joint la liste des clients en cours de traitement."; 
         public const string RDV_BODY = "Veuillez trouvez ci-joint la liste des nouveaux rendez-vous."; 
-        
         public const string VERSEMENT_SUBJECT = "Liste des versements";
         public const string AVERIFIE_SUBJECT = "Liste des clients à verifier";
         public const string ENCOURS_SUBJECT = "Liste des clients en cours de traitement";
         public const string RDV_SUBJECT = "Liste des rendez-vous";
-       
-        public const string TO = "becem.tlych@banquezitouna.com,Riadh.BenHamza@banquezitouna.com,Hedi.Zabi@banquezitouna.com";
+        public const string TO = "Hedi.Zabi@banquezitouna.com,becem.tlych@banquezitouna.com,Riadh.BenHamza@banquezitouna.com";
 
     }
 }
